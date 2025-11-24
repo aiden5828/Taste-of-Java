574457f096cbf45b4f7ef0da0d11f6ae83c1aff5
aiden5828 <Mills.Aiden@student.greenriver.edu>
2025-11-24 11:50:49 -0800

    11/24

---
+++ buttons.js
@@ -0,0 +1,24 @@
+/* 
+blah
+blah 
+*/
+
+// blah blah blah
+
+// Sends a message on loading
+console.log("Hello!!");
+
+function logger() {
+    console.log("AMAZING");
+    console.log("YOU PRESSED THE BUTTON");
+}
+
+function alerter() {
+    // console.log("ALERT");
+    alert("ALERT");
+}
+
+function toggledark() {
+    // console.log("DARKMODE");
+    document.body.classList.toggle("darkmode")
+}
